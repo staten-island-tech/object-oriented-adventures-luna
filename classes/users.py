ff47a0d666974e43f85ec0e767b7e339906ee055
christina <158211764+christinac09@users.noreply.github.com>
2024-04-09 13:01:17 -0400

    ?????

--- classes/users.py
+++ classes/users.py
@@ -1,8 +1,13 @@
+import json
+import os
+
+
 class user():
     def __init__(self, username, password, type):
         self.username = username
         self.passward = password
         self.type = type
+
 
 class player(user):
     def __init__(self, username, password, type, version, team_lvl, crystals, stars):
@@ -17,4 +22,29 @@ class admin(user):
         super().__init__(username, password, type)
         self.rank = rank
 
-def add():
+with open("users.json", "r") as f:
+    users = json.load(f)
+    
+    def add_user(username, password, type):
+        new_user = user(username, password, type)
+        users.append(new_user.__dict__)
+
+    continue_add = "Y"
+    while continue_add == "Y":
+        user_request = input("What type of user? player or admin ").upper()
+        if user_request == "PLAYER":
+            username = input("enter username: ")
+            password = input("enter password: ")
+        type = input("enter type: ")
+        add_user(username, password, type)
+        continue_add = input(" want to add another user? Y/N ").upper()
+
+
+
+new_file = "updated.json"
+with open(new_file, "w") as f:
+    json_string = json.dumps(users)
+    f.write(json_string)
+
+os.remove("users.json")
+os.rename(new_file, "users.json")
