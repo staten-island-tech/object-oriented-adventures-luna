45030f1079c730a918e80143ed48899d52440a81
christina <158211764+christinac09@users.noreply.github.com>
2024-05-01 13:32:26 -0400

    Update login.py

--- game files/login.py
+++ game files/login.py
@@ -26,21 +26,5 @@ class login():
         print("Congrats! You have successfully logged in ")
         # and then load that user's data or something idkidkidk
 
-"""     def admin():
-        username = input("Enter admin username: ")   # do i even need to make a separate one for admin
-        num_matches = []
-        for user in data:
-            if username in user["username"]:
-                num_matches.append(user)
-                password = input(f"Enter password for {username}: ")
-        if len(num_matches) == 0:
-            print("This username does not exist. Please try again ")
-            login.player()
-        for i in num_matches:       # works bc there's only one dictionary in the list since only one username should match
-            while i['password'] != password:
-                print("incorrect password, try again")
-                password = input(f"enter password for {username}: ")
-        print("Congrats! You have successfully logged in ")
-        # and then load that user's data or something idkidkidk """
     
-login.player()  # function works in this file, dk to how incorporate data from users.json
+login.player()  
