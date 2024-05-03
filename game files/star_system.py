6729e26bc779e5e03c76c8e2037af4bbcbb6ac75
christina <158211764+christinac09@users.noreply.github.com>
2024-05-03 13:51:01 -0400

    jlkj

---
+++ game files/star_system.py
@@ -0,0 +1,25 @@
+
+#import stuff
+import json
+import os
+users = open(r"\object-oriented-adventures-luna\classes\users.json", encoding="utf8")
+user_data = json.load(users)
+
+class star():
+    def pull_one(user):
+        #subtract 160 crystals from user
+        #if not enough, go to do mission
+        #choose random character, use rates
+        # check for dupes: if yes, add 80 crystals
+        # if no, append character to user data
+        pass
+
+    def pull_ten(user):
+        # subtract 1600 crystals
+        #if not enough, go to do mission
+        #choose random character 10 times using while loop or something
+        #check for dupe each time: if yes add 80 crystals
+        #if no append to user data
+        pass
+
+    # HOW DOES PITY WORK !!!   
