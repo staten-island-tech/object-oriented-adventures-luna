5364a9d06e0350672bc44b3eff7b58ded6022c5c
christina <158211764+christinac09@users.noreply.github.com>
2024-05-09 13:58:22 -0400

    Update star_system.py

--- game files/star_system.py
+++ game files/star_system.py
@@ -2,9 +2,8 @@
 #import stuff
 
 import json
-# Open the JSON file of pokemon data
-test = open(r"C:\Users\christina.chen23\Documents\GitHub\object-oriented-adventures-luna\game files\classes\entities.json", encoding="utf8")
-# create variable "data" that represents the enitre pokedex list
+import random
+test = open(r"C:\Users\christina.chen23\Documents\GitHub\object-oriented-adventures-luna\game files\classes\json\entities.json", encoding="utf8")
 entities = json.load(test)
 
 users = [
@@ -43,7 +42,6 @@ users = [
 star_counter = 0
 
 class star():
-    import random
     
     def soft_pity():
         while star_counter == 70:
@@ -58,24 +56,20 @@ class star():
             crystals = user['crystals']
             print(f"{usernam} currently has {crystals} crystals")
             break
-        if crystals > 160:
-            crystals -= 160
-            print(f"{usernam} now has {crystals} crystals left")
-        else:
+        while crystals < 160:
             print("You do not have enough crystals.")
-        rare = []
-        super_rare = []
-        for i in entities:
-            if i["rarity"] == "****":
-                rare.append(i)
-            else:
-                super_rare.append(i)
-        rates = [0.85875, 0.14125*0.99125, 0.14125*0.00875]
+        crystals -= 160
+        print(f"{usernam} now has {crystals} crystals left")
+            
+        """ 
+        r = [85.875, 14.125*0.99125, 14.125*0.00875]
+        results = random.choices(("nothing", ), weights=(r[0],r[1],r[2]), k=1) """
 
-    
-
+        results = random.choice(entities)
+        user['character'].append(results)
+        print(user['character'])
         
-         #subtract 160 crystals from user - DONE
+         #subtract 160 crystals from user
         #if not enough, go to do mission
         #choose random character, use rates
         # check for dupes: if yes, add 80 crystals
@@ -83,12 +77,30 @@ class star():
 
         
 
-    def pull_ten(user):
+    def pull_ten(usernam):
+        a = 0
+        for user in users:
+            while user['username'] != usernam:
+               a += 1
+            crystals = user['crystals']
+            print(f"{usernam} currently has {crystals} crystals")
+            break
+        while crystals < 1600:
+            print("You do not have enough crystals.")
+        crystals -= 1600
+        print(f"{usernam} now has {crystals} crystals left")
+            
+        """ 
+        r = [85.875, 14.125*0.99125, 14.125*0.00875]
+        results = random.choices(("nothing", ), weights=(r[0],r[1],r[2]), k=1) """
+
+        results = random.choice(entities, k=10)
+        user['character'].append(results)
         # subtract 1600 crystals
         #if not enough, go to do mission
-        #choose random character 10 times using while loop or something
+        #choose random character 10 times
         #check for dupe each time: if yes add 80 crystals
         #if no append to user data
-        pass
+        
 
 star.pull_one("example")
