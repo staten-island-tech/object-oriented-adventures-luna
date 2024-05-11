179f8cbe14fd02e713b3d9e994e91e715e749392
christina <christinac272@nycstudents.net>
2024-05-11 16:42:43 -0400

    Update star_system.py

--- game files/star_system.py
+++ game files/star_system.py
@@ -13,7 +13,7 @@ users = [
         "type": "player",
         "version": 0,
         "team_lvl": 1,
-        'crystals': 500,
+        'crystals': 200,
         "stars": 0,
         "character": []
     },
@@ -56,42 +56,46 @@ class star():
             crystals = user['crystals']
             print(f"{usernam} currently has {crystals} crystals")
             break
-        while crystals < 160:
+        if crystals < 160:
             print("You do not have enough crystals.")
-        answer = input("are you sure you want to use 160 crystals? y/n").lower()
-        while answer != "n" and answer != "y":
-            answer = input("are you sure you want to use 160 crystals? y/n").lower()
-        while answer == "n":
-            break
         else: 
-            crystals -= 160
-            print(f"{usernam} now has {crystals} crystals left")
-            character = random.choices(entities)
-            list_b = [random.randint(1,100) for i in range(15)]       #generates a list of 15 random int btwn 1-100
-            b = random.randint(1,100)           # generate 1 random int btwn 1-100
-            if b in list_b:                     #15% chance of occurring
-                if character["rarity"] == "*****":
-                    the_number = random.randint(1,100)
-                    c = random.randint(1,100)                 #1 percent chance of this happening
-                    if c in the_number:
-                        print(f"Congrats! You pulled a five-star character")
-                        print("")
-                        print(character)
-                        user['character'].append(character)
+            answer = input("are you sure you want to use 160 crystals? y/n ").lower()
+            while answer != "n" and answer != "y":
+                answer = input("are you sure you want to use 160 crystals? y/n ").lower()
+            while answer == "y":
+                
+                crystals -= 160
+                character = random.choices(entities).pop()
+                print(character)
+                list_b = [random.randint(1,100) for i in range(15)]       #generates a list of 15 random int btwn 1-100
+                b = random.randint(1,100)           # generate 1 random int btwn 1-100
+                if b in list_b:                     #15% chance of occurring
+                    
+                    if character['rarity'] == "*****":
+                        the_number = random.randint(1,100)
+                        c = random.randint(1,100)                 #1 percent chance of this happening
+                        if c == the_number:
+                            print(f"Congrats! You pulled a five-star character")
+                            print(character)
+                            user['character'].append(character)
+                        else:
+                            print("oh no! you lost ur five-star :(")
                     else:
-                        print("oh no! you lost ur five-star :(")
-                else:
-                    the_number = [random.randint(1,100) for i in range(99)]
-                    c = random.randint(1,100)
-                    if c in the_number:
-                        print(f"Congrats! You pulled a four-star character")
-                        print("")
-                        print(character)
-                        user['character'].append(character)
-            else: 
-                print("you got nothing hahahahah")
-            
+                        the_number = [random.randint(1,100) for i in range(99)]
+                        c = random.randint(1,100)
+                        if c in the_number:
+                            print(f"Congrats! You pulled a four-star character")
+                            print(character)
+                            user['character'].append(character)
+                else: 
+                    print("you got nothing hahahahah")
+                retry = input("pull again? y/n ").lower()
+                while retry != "y" and retry != "n":
+                    retry = input("pull again? y/n ").lower()
+                while retry == "y":
+                    star.pull_one(usernam)
 
+        print(f"{usernam} now has {crystals} crystals left")
         print(f"Your current characters: {user['character']}")
         
          #subtract 160 crystals from user
@@ -122,4 +126,4 @@ class star():
         #if no append to user data
         
 
-star.pull_one("exampleee")
+star.pull_one("example")
