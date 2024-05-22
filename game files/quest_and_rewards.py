226eb9b84c58d7678b1dcc180f370311d7c11ff0
elisa-23 <elisac40@nycstudents.net>
2024-05-22 11:54:23 -0400

    Update quest_and_rewards.py

--- game files/quest_and_rewards.py
+++ game files/quest_and_rewards.py
@@ -1,9 +1,21 @@
 class quests():
     def rewards(x):
-        pass
+        if x == "world":
+            pass
+        else:
+            pass
     def win(x):
         pass
     def lose(x):
         pass
     def enemy(x):
-        pass
+        if x == "monde":
+            pass
+        elif x == "pero":
+            pass
+        elif x == "taiyo":
+            pass
+        elif x == "starter":
+            pass
+        elif x == "choose":
+            pass
