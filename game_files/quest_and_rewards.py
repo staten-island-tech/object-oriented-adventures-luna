64851deabebfc12d790b05e1ff8fa7f7d64f6dda
elisa-23 <elisac40@nycstudents.net>
2024-05-23 12:29:24 -0400

    a

--- game_files/quest_and_rewards.py
+++ game_files/quest_and_rewards.py
@@ -1,13 +1,42 @@
-class quests():
-    def rewards(x,username):
+import json
+import os
+
+with open (r"game_files\classes\json\users.json", "r") as hi : 
+    users = json.load(hi)
+
+with open (r"game_files\classes\json\entities.json", "r") as bye :
+    entities = json.load(bye)
+
+class quests():                                             #the whole class needs to be tested
+    def rewards(x, username):
         if x == "world":
             rewards = 80
         else:
             rewards = 10
-    def win(x):
-        pass
-    def lose(x):
-        pass
+        for user in users:
+            if user['name'] == username:
+                crystals = user['crystals']
+                add = crystals + rewards
+                user['crystals'] = add
+        new_file = "updated.json"
+        with open(new_file, "w") as f:
+            json_string = json.dumps(users)
+            f.write(json_string)
+        os.remove(r"game_files\classes\json\users.json")
+        os.rename(new_file, r"game_files\classes\json\users.json") 
+    def lose(team, username):
+        a = 0
+        for character in team:
+            if character['hp'] == 0:
+                a += 1
+        if a == 4:
+            print(f"{username}, you have lost this battle. You will now be transported back to the spaceship for further treatment.")
+    def win(enemies, username):
+        a = []
+        for enemy in enemies:
+            a.append(enemy['hp'])
+        if sum(a) == 0:
+            print(f"{username}, you have passed this hurdle. May your journey be rich in prosperity and sopistication.")
     def enemy(x):
         if x == "monde":
             enemy = []
@@ -19,6 +48,8 @@ class quests():
             enemy = []
         elif x == "choose":
             enemy = []
-    def wave(x):
+    def select_wave(x):
+        pass
+    def wave():
         pass
     
