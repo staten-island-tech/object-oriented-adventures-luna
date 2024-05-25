5058f1a9a9343c01bda75c043819aa2052a9ba30
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2024-05-25 08:35:46 -0400

    Update prototype_1.py

--- game_files/prototype/prototype_1.py
+++ game_files/prototype/prototype_1.py
@@ -1,2 +1,15 @@
+From entry import *
+
 class prototype():
-    pass
+    def start():
+        print("Welcome to Legacy!")
+        a = input("Is your terminal in full screen? y/n").lower()
+        b = ["y", "n"]
+        while a not in b:
+            a = input("Is your terminal in full screen? y/n").lower()
+        while a == "n":
+            print("Please put your terminal in full screen.")
+            a = input("Is your terminal in full screen now? y/n").lower()
+        while a == "y":
+            pass
+            
