335b9a82e0eddb64d7edb9d812779a7fd71318f6
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2024-05-25 15:28:50 -0400

    Update spaceship.py

--- game_files/spaceship.py
+++ game_files/spaceship.py
@@ -1,13 +1,24 @@
 with open (r"game_files\classes\json\users.json", "r") as hi : 
     users = json.load(hi)
 
+import sys
+sys.path.append(gamefile)
+from random import *
+import os
+
 class spaceship():
-    def welcome():
+    def welcome(username):
         print("??: Welcome abroad the spaceship! Captain Xingxing, here!")
-        c = input("[A] Continue").upper()
-            while c != "A":
-                c = input("").upper()
+        random.contin()
         print("Xingxing: Greetings! Is there anything you would like?")
+        print([1] Quests)
+        for user in users:
+            if user['name'] == username:
+                data = len(user['quest'])
+        if data == 2:
+            print("[2] Star System")
+            print("[3] Change Team")
+        answer = input("")
         
     def quest(username):
         for user in users:
