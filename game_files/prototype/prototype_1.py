c25ac2d9ba792dca074e22fb22586f1c6de98387
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2024-05-25 10:24:13 -0400

    Update prototype_1.py

--- game_files/prototype/prototype_1.py
+++ game_files/prototype/prototype_1.py
@@ -1,4 +1,17 @@
-From entry import *
+
+## call in the json files and other programs(login and loading screen)
+import sys
+import os
+sys.path.append(game_files/classes)
+import login
+sys.path
+
+with open (r"game_files\classes\json\users.json", "r") as hi : 
+    users = json.load(hi)
+
+with open (r"game_files\classes\json\entities.json", "r") as bye :
+    entities = json.load(bye)
+
 
 class prototype():
     def start():
@@ -11,5 +24,23 @@ class prototype():
             print("Please put your terminal in full screen.")
             a = input("Is your terminal in full screen now? y/n").lower()
         while a == "y":
-            entry.
+            os.system("cls")
+            entry.loading()
+            c = input("[A] Continue").upper()
+            while c != "A":
+                c = input("")
+            os.system("cls")
+            print("Welcome to our game! Would you like to sign in to your existing account or sign up for a new account?")
+            print("[1] Sign in")
+            print("[2] Sign up")
+            account = input("")
+            d = ["1", "2"]
+            while account not in d:
+                print("[1] Sign in")
+                print("[2] Sign up")
+                account = input("")
+            if account == "1":
+                
+            
+            
             
