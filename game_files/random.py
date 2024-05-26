a308170a94156be6872458483aacf1591349fda7
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2024-05-26 13:51:40 -0400

    Update random.py

--- game_files/random.py
+++ game_files/random.py
@@ -1,5 +1,19 @@
 class random():
   def contin():
     c = input("[A] Continue").upper()
-            while c != "A":
-                c = input("").upper()
+      while c != "A":
+      c = input("").upper()
+  def load():
+    for i in range(35):
+      print("loading '")
+      os.system("cls")
+      print("loading !")
+      os.system("cls")
+      print("loading ,")
+      os.system("cls")
+      print("loading .")
+      os.system("cls")
+      print("loading ;")
+      os.system("cls")
+      print("loading *")
+      os.system("cls")
