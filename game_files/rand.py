9c35540a737b339986cef4c7576b4f3474a5088c
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2024-05-28 16:27:08 -0400

    Update rand.py

--- game_files/rand.py
+++ game_files/rand.py
@@ -3,6 +3,7 @@ class random():
     c = input("[A] Continue").upper()
     while c != "A":
       c = input("").upper()
+    os.system("cls")
   def load():
     for i in range(35):
       print("loading '")
