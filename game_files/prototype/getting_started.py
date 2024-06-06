28be1de792419c484112592d096fc9baac014e51
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2024-06-06 14:44:49 -0400

    Update getting_started.py

--- game_files/prototype/getting_started.py
+++ game_files/prototype/getting_started.py
@@ -122,7 +122,7 @@ class prologue():
                 for i in range(3):
                     enemy_team.append(en)
         for enemy in enemy_team:
-            b.append(enemy['hp']
+            b.append(enemy['hp'])
         while ally_hp <= 0 and enemy_hp <= 0:
             battle.cycle(z, enemy_team, team)
             b = []
@@ -154,6 +154,7 @@ class prologue():
         os.remove(r"game_files\classes\json\users.json")
         os.rename(new_file, r"game_files\classes\json\users.json") 
         print("You've gained 3 crystals")
+        rand.contin()
         dialogues_story.space_station(8)
         rand.contin()
         dialogues_player.space_station(5)
@@ -183,7 +184,7 @@ class prologue():
         z = team[y]
         y = 0
         while wave != 2:
-            quests.wave(wave, 'spaceship')
+            quests.wave(wave, 0)
             enemy_team = []
             for enemy in entity:
                 if enemy['name'] == "Oblivion Guard":
@@ -192,6 +193,51 @@ class prologue():
                 elif enemy['name'] == "Oblivion Drones":
                     for i in range(2):
                         enemy_team.append(en)
+            for enemy in enemy_team:
+                b.append(enemy['hp'])
+            while ally_hp <= 0 and enemy_hp <= 0:
+                battle.cycle(z,enemy_team, team)
+                b = []
+                for en in enemy_team:
+                b.append(en['hp'])
+                battle.attack_enemy(enemy_team, team)
+                a = []
+                for character in team:
+                    a.append(character['hp'])
+                ally_hp = sum(a)
+                enemy_hp = sum(b)
+                y += 1
+                if y > l - 1:
+                    y = 0
+                if ally_hp <= 0:
+                    quests.lose(username, team)
+                    prologue.path(username)
+                else:
+                    reward = 7
+                    for user in users:
+                        if user['username'] == username:
+                            crystals = user['crystals']
+                            add = crystals + rewards
+                            print(f"{username} now has {add} crystals")
+                            user['crystals'] = add
+                    new_file = "updated.json"
+                    with open(new_file, "w") as f:
+                        json_string = json.dumps(users)
+                        f.write(json_string)
+                    os.remove(r"game_files\classes\json\users.json")
+                    os.rename(new_file, r"game_files\classes\json\users.json") 
+                    wave += 1
+            print("You look around to see that there are no more monsters. As you let your guard down you see a shadow behind you.")
+            rand.contin()
+            wave = 0
+            quests.wave(wave, 0)
+            enemy_team = []
+            for enemy in entity:
+                if enemy['name'] == "Oblivion Drones":
+                    for i in range(2):
+                        enemy_team.append(en)
+            for enemy in enemy_team:
+                b.append(enemy['hp'])
             while ally_hp <= 0 and enemy_hp <= 0:
                 battle.cycle(z,enemy_team, team)
                 b = []
@@ -214,6 +260,108 @@ class prologue():
                 for user in users:
                     if user['username'] == username:
                         crystals = user['crystals']
+                        add = crystals + rewards
+                        print(f"{username} now has {add} crystals")
+                        user['crystals'] = add
+                new_file = "updated.json"
+                with open(new_file, "w") as f:
+                    json_string = json.dumps(users)
+                    f.write(json_string)
+                os.remove(r"game_files\classes\json\users.json")
+                os.rename(new_file, r"game_files\classes\json\users.json") 
+            wave += 1
+            quests.wave(wave, 1)
+            enemy_team = []
+            for enemy in entity:
+                if enemy['name'] == "General Aeron":
+                    enemy_team.append(en)
+            for enemy in enemy_team:
+                b.append(enemy['hp'])
+            while ally_hp <= 0 and enemy_hp <= 0:
+                battle.cycle(z,enemy_team, team)
+                b = []
+                for en in enemy_team:
+                b.append(en['hp'])
+                battle.attack_enemy(enemy_team, team)
+                a = []
+                for character in team:
+                    a.append(character['hp'])
+                ally_hp = sum(a)
+                enemy_hp = sum(b)
+                y += 1
+                if y > l - 1:
+                    y = 0
+            if ally_hp <= 0:
+                quests.lose(username, team)
+                prologue.path(username)
+            else:
+                reward = 40
+                for user in users:
+                    if user['username'] == username:
+                        crystals = user['crystals']
+                        add = crystals + rewards
+                        print(f"{username} now has {add} crystals")
+                        user['crystals'] = add
+                new_file = "updated.json"
+                with open(new_file, "w") as f:
+                    json_string = json.dumps(users)
+                    f.write(json_string)
+                os.remove(r"game_files\classes\json\users.json")
+                os.rename(new_file, r"game_files\classes\json\users.json")
+            rand.contin()
+            dialogues_story.space_station(10)
+            rand.contin()
+            dialogues_player.space_station(7)
+            rand.contin()
+            a = input("").lower()
+            b = ["a", "b"]
+            while a not in b:
+                print("You think it over before you speak and decide that it is not the right thing to say at the moment.")
+                rand.contin()
+                print("You try and think of another response.")
+                dialogues_player.space_station(7)
+                a = input("").lower()
+            rand.contin()
+            dialogues_story.space_station(11)
+            rand.contin()
+            dialogues_player.space_station(8)
+            a = input("").lower()
+            while a not in b:
+                print("You think it over before you speak and decide that it is not the right thing to say at the moment.")
+                rand.contin()
+                print("You try and think of another response.")
+                dialogues_player.space_station(8)
+                a = input("").lower()
+            rand.contin()
+            dialogues_story.space_station(12)
+            rand.contin()
+            dialogues_player.space_station(9)
+            a = input("").lower()
+            while a not in b:
+                print("You think it over before you speak and decide that it is not the right thing to say at the moment.")
+                rand.contin()
+                print("You try and think of another response.")
+                dialogues_player.space_station(9)
+                a = input("").lower()
+                rand.contin()
+            if a == b[0]:
+                dialogues_story.space_station(13)
+                rand.contin()
+                dialogues_story.space_station(16)
+                rand.contin()
+                print("Great Job! You finished the first story quest!!")
+            elif a == b[1]:
+                dialogues_story.space_station(14)
+                rand.contin()
+                dialogues_story.space_station(15)
+                rand.contin()
+                print("Great Job! You finished the first story quest!!")
+                print("Your Journey has ended...")
+                print("You have not found your sibling, but you may continue playing to find out what would have happened if you chose the other route.")
+            reward = 1600
+            for user in users:
+                if user['username'] == username:
+                    crystals = user['crystals']
                     add = crystals + rewards
                     print(f"{username} now has {add} crystals")
                     user['crystals'] = add
@@ -222,6 +370,8 @@ class prologue():
                 json_string = json.dumps(users)
                 f.write(json_string)
             os.remove(r"game_files\classes\json\users.json")
-            os.rename(new_file, r"game_files\classes\json\users.json") 
-            
+            os.rename(new_file, r"game_files\classes\json\users.json")
+            rand.contin()
+            print("Going to spaceship...")
+            rand.load()
 prologue.path('exa')
