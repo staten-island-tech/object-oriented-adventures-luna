b7864319da1cccac8aa0b10e71653a98bfe188f5
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2024-06-06 16:03:45 -0400

    Update worlds.py

--- game_files/prototype/worlds.py
+++ game_files/prototype/worlds.py
@@ -15,17 +15,31 @@ with open (r"game_files\classes\json\entities.json", "r") as bye :
     entities = json.load(bye)
 
 class worlds():
-  def monde_mission(username):
-      dialogues_story.monde(0)
-      rand.contin()
-      dialogues_player.monde(0)
-      a = input("").lower()
-      b = ["a", "b"]
-      while a not in b:
-          print("You think it over before you speak and decide that it is not the right thing to say at the moment.")
-          rand.contin()
-          print("You try and think of another response.")
-          dialogues_player.monde(0)
-          a = input("").lower()
-     rand.contin()
-     
+	def monde_mission(username):
+		dialogues_story.monde(0)
+		rand.contin()
+		dialogues_player.monde(0)
+		a = input("").lower()
+		b = ["a", "b"]
+		while a not in b:
+			print("You think it over before you speak and decide that it is not the right thing to say at the moment.")
+			rand.contin()
+			print("You try and think of another response.")
+			dialogues_player.monde(0)
+			a = input("").lower()
+		rand.contin()
+		dialogues_story.monde(1)
+		rand.contin()
+		dialogues_story.monde(2)
+		rand.contin()
+		dialogues_player.monde(1)
+		a = input("").lower()
+		while a not in b:
+			print("You think it over before you speak and decide that it is not the right thing to say at the moment.")
+			rand.contin()
+			print("You try and think of another response.")
+			dialogues_player.monde(0)
+			a = input("").lower()
+		rand.contin()
+		dialogues_story.monde(3)
+		rand.contin()
